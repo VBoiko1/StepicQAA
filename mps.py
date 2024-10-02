ebd730b595732624fbf2bc71ab3e680898f4050e
VBoiko1 <vboiko891@gmail.com>
2024-10-03 00:19:11 +0300

    sggfs

--- mps.py
+++ mps.py
@@ -10,11 +10,11 @@ from selenium.webdriver.common.by import By
 driver = webdriver.Chrome()
 
 # команда time.sleep устанавливает паузу в 15 секунд, чтобы мы успели увидеть, что происходит в браузере
-time.sleep(10)
+time.sleep(3)
 
 # Метод get сообщает браузеру, что нужно открыть сайт по указанной ссылке
 driver.get("https://survey-service-trunk.rts-tender.ru/")
-time.sleep(10)
+time.sleep(3)
 
 # Метод find_element позволяет найти нужный элемент на сайте, указав путь к нему. 
 # Метод принимает в качестве аргументов способ поиска и значение, по которому мы будем искать
@@ -24,36 +24,50 @@ textarea = driver.find_element(By.XPATH, "//input[@class='p-element ng-tns-c45-3
 
 # Напишем текст ответа в найденное поле "Организация"
 textarea.send_keys("9723098918")
-time.sleep(10)
+time.sleep(3)
 
 # Выбор организации из выподающего списка
 submit_button = driver.find_element(By.CSS_SELECTOR, "div.ng-star-inserted")
 submit_button.click()
-time.sleep(10)
+time.sleep(3)
 
 # Ищем поле "Заполнил" для ввода текста
 textarea = driver.find_element(By.XPATH, "//input[@placeholder='ФИО сотрудника']")
 textarea.send_keys("Иванов Иван Иванович")
-time.sleep(10)
+time.sleep(3)
 
 # Найти галлочку выпадающего списка поля "Место заполнения анкеты"
 submit_button = driver.find_element(By.XPATH, "//span[@class='p-dropdown-trigger-icon pi pi-chevron-down']")
 
 # Нажать на галочку
 submit_button.click()
-time.sleep(10)
+time.sleep(3)
 
 # Выбор "Места заполнения анкеты" по платформе из списка
 submit_button = driver.find_element(By.XPATH, "//li[@aria-label='ЛК 44']")
 
 # Нажать на выбранную организацию
 submit_button.click()
-time.sleep(10)
+time.sleep(3)
 
 # Поиск кнопки "Добавить товар"
-textarea = driver.find_element(By.XPATH, "//rts-icon > svg-icon > svg[@width='24']")
+textarea = driver.find_element(By.XPATH, "//*[contains(text(),'Добавить товар')]") 
+
+# Нажать на кнопку "Добавить товар"
 textarea.click()
-time.sleep(10)
+time.sleep(3)
+
+#Поиск кноки "Загрузка файла с ТРУ"
+#textarea = driver.find_element(By.XPATH, "//*[contains(text(),'Загрузить файл с ТРУ')]")
+
+# Выбрать файл для загрузки
+
+
+# Поиск кнопки "Загрузить список товаров"
+textarea = driver.find_element(By.XPATH, "//*[contains(text(),'Загрузить список товаров')]")
+
+textarea.click()
+time.sleep(3)
 
 
 
