045df3cf53ddd82eb0c6f6008067d28c3f005d36
VBoiko1 <vboiko891@gmail.com>
2024-10-03 12:35:53 +0300

    фвафвыа

---
+++ lesson_ssilka_poisk.py
@@ -0,0 +1,22 @@
+import math
+
+from selenium import webdriver
+
+import time
+
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+
+driver.get("http://suninjuly.github.io/find_link_text")
+time.sleep(5)
+
+# link = driver.find_element(By.LINK_TEXT, str(math.ceil(math.pow(math.pi, math.e)*10000)))
+# link.click()
+# time.sleep(10)
+
+link = driver.find_element(By.LINK_TEXT,'224592')
+link.click()
+time.sleep(10)
+
+driver.quit()
